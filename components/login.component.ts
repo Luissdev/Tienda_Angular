f75822b4abeb97494aef2faa975b3f9e02884627
Luis <luisg9410@gmail.com>
2016-10-22 23:34:15 -0500

    Inicio
    - Comienzo del proyecto

---
+++ components/login.component.ts
@@ -0,0 +1,12 @@
+// Importar el núcleo de Angular
+import {Component, OnInit} from '@angular/core';
+ 
+// Decorador component, indicamos en que etiqueta se va a cargar la 
+
+@Component({
+    selector: 'login',
+    template: '<h1>Formulario de login</h1>'
+})
+ 
+// Clase del componente donde irán los datos y funcionalidades
+export class LoginComponent { }
